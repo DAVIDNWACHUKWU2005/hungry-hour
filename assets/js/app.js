ae6f8f979192ce8b48eaf35a177f4fa416ba0880
david <davidike444@gmail.com>
2024-12-11 23:47:37 -0600

    added map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,39 @@
 "use strict";
 
 const btn = document.querySelector("button");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRud2FjaHVrd3UiLCJhIjoiY200a3ZuZHN5MHRkbzJqbzlkbmFuZms2ZCJ9.MvyhkYEwfB2Sjft0jAofwQ';
+const map = new mapboxgl.Map({
+    container: 'map', 
+    style: 'mapbox://styles/mapbox/streets-v11', 
+    center: [0, 0], 
+    zoom: 5 
+  });
+
+
+  function getLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    
+    map.flyTo({
+      center: [longitude, latitude], 
+      zoom: 14,  
+      essential: true 
+    });
+  }
+
+  const accuracy = {
+    enableHighAccuracy: true
+  };
+
+  btn.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getLocation, (error) => {
+        alert("Unable to retrieve your location.");
+        console.error(error);
+      }, accuracy);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  });
